063b2cdd97837d91854304d329ea51065c7f973b
FelipeFVCampidelli <felipefvcampidelli@hotmail.com>
2021-06-24 03:33:33 -0300

    Rotas/Dependencias/imagens

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import axios from 'axios';
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
 import './App.css';
 
+
 function App() {
+  axios.defaults.baseURL = process.env.REACT_APP_API_URL;
   return (
     <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
