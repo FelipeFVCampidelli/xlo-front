b98743933e19d941754cfe5bc8ce5178bd6b02bc
FelipeFVCampidelli <felipefvcampidelli@hotmail.com>
2021-06-24 03:56:27 -0300

    Visual Login

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,20 @@
+import './Login.css';
+import Welcome from './Welcome/Welcome';
+import Form from './Form/Form';
+
+
+export default function Login() {
+
+  return (
+    <div className="Login">
+      <section className="container">
+        <div className="left">
+          <Welcome />
+        </div>
+        <div className="right">
+          <Form />
+        </div>
+      </section>
+    </div>
+  )
+}
